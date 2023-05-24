9e6f1f4510cb20358381e9b97580d2eb6868159e
hoangnhan2003 <ngohoangnhan03@gmail.com>
2023-05-24 17:55:45 +0700

    update-login-nhanNH

--- angular-pawn-app/src/app/app.module.ts
+++ angular-pawn-app/src/app/app.module.ts
@@ -3,22 +3,26 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {ArticleModule} from "./modules/article/article.module";
-import {CategoryModule} from "./modules/category/category.module";
-import {ContractModule} from "./modules/contract/contract.module";
-import {CustomerModule} from "./modules/customer/customer.module";
-import {EmployeeModule} from "./modules/employee/employee.module";
-import {ImageModule} from "./modules/image/image.module";
-import {ProductModule} from "./modules/product/product.module";
-import {RoleModule} from "./modules/role/role.module";
-import {StatusModule} from "./modules/status/status.module";
-import {UserModule} from "./modules/user/user.module";
-import {UserHasRoleModule} from "./modules/user-has-role/user-has-role.module";
-import {FinaceModule} from "./modules/finace/finace.module";
+import {ArticleModule} from './modules/article/article.module';
+import {CategoryModule} from './modules/category/category.module';
+import {ContractModule} from './modules/contract/contract.module';
+import {CustomerModule} from './modules/customer/customer.module';
+import {EmployeeModule} from './modules/employee/employee.module';
+import {ImageModule} from './modules/image/image.module';
+import {ProductModule} from './modules/product/product.module';
+import {RoleModule} from './modules/role/role.module';
+import {StatusModule} from './modules/status/status.module';
+import {UserModule} from './modules/user/user.module';
+import {UserHasRoleModule} from './modules/user-has-role/user-has-role.module';
+import {FinaceModule} from './modules/finace/finace.module';
+import { HomeComponent } from './modules/home/home.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from './service/auth/auth.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +40,11 @@ import {FinaceModule} from "./modules/finace/finace.module";
     UserHasRoleModule,
     FinaceModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ angular-pawn-app/src/app/service/auth-guard.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs/internal/Observable';
+import {AuthClientService} from './auth-client.service';
+import {UserRole} from '../models/UserRole';
+import {JwtService} from './jwt.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+  constructor(private authService: AuthClientService, private router: Router, private jwtClientService: JwtService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // const logged = this.authService.getIsLogged();
+    const logged = this.jwtClientService.verifyToken();
+    console.log('logged: ' + logged);
+    if (!logged) {
+      this.router.navigateByUrl('/user/login');
+      return false;
+    } else {
+      const userRole: UserRole[] = [];
+      const roles = localStorage.getItem('roles').split(',');
+      roles.forEach(role => {
+        if (role === 'ADMIN') {
+          userRole.push(UserRole.Admin);
+        } else {
+          userRole.push(UserRole.User);
+        }
+      });
+      console.log('Role: ' + userRole);
+      const requiredRoles: any = route.data.roles as UserRole[];
+      console.log('type userRole ' + typeof userRole);
+      console.log('required role: ' + requiredRoles);
+      console.log('!requiredRoles.includes(userRole): ' + userRole.includes(requiredRoles));
+      if (requiredRoles && requiredRoles.length > 0 && !requiredRoles.some(role => userRole.includes(role))) {
+        // Người dùng không có quyền truy cập vào route, điều hướng tới trang lỗi hoặc trang không được phép
+        // this.router.navigate(['/error']);
+        console.log('Khong co quyen');
+        return false;
+      }
+      return true;
+    }
+  }
+}
