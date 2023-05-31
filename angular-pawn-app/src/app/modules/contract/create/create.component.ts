7f6288ccb38fbfb161077b3c8bccd15a4733f531
Your Name <thuonglx6@gmail.com>
2023-05-31 23:36:50 +0700

    new 23:36 31/05

--- angular-pawn-app/src/app/modules/contract/create/create.component.ts
+++ angular-pawn-app/src/app/modules/contract/create/create.component.ts
@@ -9,6 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ContractServiceService } from 'src/app/service/contract-service.service';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Customer } from 'src/app/models/customer/Customer';
+import { ValidationErrors } from '@angular/forms';
 
 
 @Component({
@@ -29,6 +30,8 @@ export class CreateComponent implements OnInit {
   file: any;
   paginationArray: number[];
   flag: boolean = false;
+  errorMessage: string;
+  
 
   customers: any[]; // Danh sách khách hàng
   currentPage: number = 0; // Trang hiện tại
@@ -58,34 +61,26 @@ export class CreateComponent implements OnInit {
     this.categoryService.getAll().subscribe(next => { this.categories = next;});
     this.statusService.getAll().subscribe(next => { this.statuses = next;});
     }
-
-
+  
+  
   contractForm: FormGroup = new FormGroup({
-    nameProduct: new FormControl('', [Validators.required]),
-    beginDate: new FormControl('', [Validators.required]),
-    endDate: new FormControl('', [Validators.required]),
-    price: new FormControl('', [Validators.required]),
-    imgPath: new FormControl('', [Validators.required]),
-    interest: new FormControl('', [Validators.required]),
+    nameProduct: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+    beginDate: new FormControl('', [Validators.required, this.validatePastDate]),
+    endDate: new FormControl('', [Validators.required, this.validatePastDate]),
+    price: new FormControl('', [Validators.required, Validators.min(500000), Validators.max(1000000000)]),
+    imgPath: new FormControl(),
+    interest: new FormControl('', [Validators.required, Validators.min(50000), Validators.max(100000000)]),
     category: new FormControl('', [Validators.required]),
     status: new FormControl('', [Validators.required]),
-    username: new FormControl(localStorage.getItem('token')),
+    username: new FormControl(localStorage.getItem('username')),
     customer: new FormControl()
-  });
+  }, [this.validateDateRange]);
+  // [this.validateDateRange]
   
+
   onFileChange($event){
     this.file = $event.target.files[0]
   }
-
-  // async uploadImg(){
-  //   // console.log(this.file)
-  //   const uploadTask = await this.fireStorage.upload("/productImg"+Math.random()+this.file, this.file);
-  //   const url = await uploadTask.ref.getDownloadURL();
-  //   // console.log(url);
-  //   this.contractForm.controls.imgPath.setValue(url);
-  //   this.contractForm.controls.username.setValue("admin1");
-  //   console.log(this.contractForm.value)
-  // }
 
   selectCustomer(customer: Customer){
     this.customer = customer;
@@ -95,17 +90,51 @@ export class CreateComponent implements OnInit {
   }
 
   async submit() {
-    // console.log(this.file)
     const uploadTask = await this.fireStorage.upload("/productImg"+Math.random()+this.file, this.file);
     const url = await uploadTask.ref.getDownloadURL();
-    // console.log(url);
     this.contractForm.controls.imgPath.setValue(url);
-    this.contractForm.controls.username.setValue("admin1");
     console.log(this.contractForm.value)
     if (this.contractForm.valid) {
-      this.contractService.saveContract(this.contractForm.value).subscribe(next =>{
-        alert("Product saved");
-        this.router.navigateByUrl('/contract')})
-    }
-  }
+      console.log("okkkkkk")
+      this.contractService.saveContract(this.contractForm.value).subscribe(
+        next => {alert("Product saved");
+                this.router.navigateByUrl('/contract')})
 }
+  }
+
+  // ,
+  //       error => {console.log(error);
+  //                   this.errorMessage = error.message})
+
+  validatePastDate(control: FormControl) {
+    const selectedDate = new Date(control.value);
+    const currentDate = new Date();
+
+    if (selectedDate < currentDate) {
+      return { pastDate: true };
+    }
+
+    return null;
+  }
+
+  validateDateRange(group: FormGroup): ValidationErrors | null {
+    const beginDate = group.get('beginDate')?.value;
+    const endDate = group.get('endDate')?.value;
+  
+    if (beginDate && endDate && beginDate > endDate) {
+      return { dateRangeInvalid: true };
+    }
+  
+    return null;
+  }
+
+  // validateDateRange(formGroup: FormGroup) {
+  //   const beginDateValue = formGroup.get('beginDate').value;
+  //   const endDateValue = formGroup.get('endDate').value;
+
+  //   if (beginDateValue > endDateValue) {
+  //     return { dateRange: true};
+  //   } 
+  //     return null;
+  //   }
+  }
--- angular-pawn-app/src/app/service/contract-service.service.ts
+++ angular-pawn-app/src/app/service/contract-service.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Contract } from '../models/contract/Contract';
 
 @Injectable({
@@ -12,5 +12,19 @@ export class ContractServiceService {
 
   saveContract(contract: Contract): Observable<any> {
     return this.httpClient.post<any>("http://localhost:8080/api/contract", contract); 
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Xử lí lỗi khi có lỗi mạng hoặc lỗi xử lí trên client-side
+      console.error('Lỗi xảy ra:', error.error.message);
+    } else {
+      // Xử lí lỗi từ phía server
+      console.error(
+        `Mã lỗi từ server: ${error.status}, ` +
+        `Thông báo lỗi: ${error.error}`);
+    }
+    // Trả về một Observable với thông tin lỗi
+    return throwError('Đã xảy ra lỗi, vui lòng thử lại sau');
   }
 }
